5cfabcbedbd98f4e0f66ec8ead0862abe5e9b562
vinnyA3 <vincent.aceto@gmail.com>
2018-02-21 16:23:11 -0500

    feat(env): initial commit

---
+++ src/index.spec.js
@@ -0,0 +1,5 @@
+// sample test
+test('is should return true ...', () => {
+  const state = true
+  expect(state).toBe(true)
+})
---
+++ webpack.config.dev.js
@@ -0,0 +1,39 @@
+import path from 'path'
+
+const config = {
+  entry: ['./src/index'],
+  context: __dirname,
+  output: {
+    filename: 'bundle.js', // temp
+    path: path.resolve(__dirname, 'public')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
+
+export default config
